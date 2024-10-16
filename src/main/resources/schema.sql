2b448970cf63f55c5f638a878969d94d1a042ef2
Selvamuthukumar S <selvamuthukumar038@gmail.com>
2024-10-16 14:41:20 +0530

    schema update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE if NOT EXISTS Tickets (
+CREATE TABLE if NOT EXISTS tickets (
     sid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     ticketerid VARCHAR(10),
     ticketername VARCHAR(50),
