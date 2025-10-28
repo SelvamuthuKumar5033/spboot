7c23151a1b6d1038e5cd8b7bd4a67f9622cdb38d
Selvamuthukumar S <selvamuthukumar038@gmail.com>
2025-10-28 16:58:25 +0530

    servletinitializer update

--- src/main/java/com/kgisl/spb/SpbApplication.java
+++ src/main/java/com/kgisl/spb/SpbApplication.java
@@ -2,11 +2,18 @@ package com.kgisl.spb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class SpbApplication {
+public class SpbApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpbApplication.class);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Spring-Boot");
